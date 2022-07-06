eecbaeca1c88174b498736eed13db57a6f008233
Michael <mi.khai.thai@gmail.com>
2022-07-06 07:50:49 +0200

    use **kwargs to define scrabble config

--- scrabble_python/scrabble.py
+++ scrabble_python/scrabble.py
@@ -5,10 +5,10 @@ from .errors import ScrabbleError
 
 
 class Scrabble:
-    def __init__(self, players: list[Player] = None, purse: Purse = None, board: Board = None, board_size: int = 15, rack_size: int = 7,  lang: str = 'fr') -> None:
-        self.lang = lang
-        self.board_size = board_size
-        self.rack_size = rack_size
+    def __init__(self, players: list[Player] = None, purse: Purse = None, board: Board = None, **scrabble_config) -> None:
+        self.lang = scrabble_config['lang']
+        self.board_size = scrabble_config['board_size']
+        self.rack_size = scrabble_config['rack_size']
 
         self.players = players
         if self.players is None:
@@ -19,7 +19,7 @@ class Scrabble:
         self.nb_players = len(self.players)
         if self.nb_players > 4:
             raise ScrabbleError('4 players max')
-        
+
         self.purse = purse
         self.board = board
         if purse is None or board is None:
--- tests/scrabble_test.py
+++ tests/scrabble_test.py
@@ -1,12 +1,14 @@
 from scrabble_python import Scrabble
 
-board_size = 15
-rack_size = 7
-lang = 'fr'
+scrabble_config = {
+    'board_size': 15,
+    'rack_size': 7,
+    'lang': 'fr'
+}
 
 
 def test_scrabble_init():
-    scrabble = Scrabble(board_size=board_size, rack_size=rack_size, lang=lang)
+    scrabble = Scrabble(**scrabble_config)
     assert len(scrabble.players) == scrabble.nb_players == 2
-    assert [len(player.rack) for player in scrabble.players] == [rack_size] * scrabble.nb_players
-    assert len(scrabble.purse) == 102 - (rack_size*scrabble.nb_players)
+    assert [len(player.rack) for player in scrabble.players] == [scrabble.rack_size] * scrabble.nb_players
+    assert len(scrabble.purse) == 102 - (scrabble.rack_size*scrabble.nb_players)
